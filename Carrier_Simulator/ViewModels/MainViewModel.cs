3b307a2c221f67ced7da6e0fa34303b24f3d3c35
Dsoooo <101105180+Dsoooo@users.noreply.github.com>
2025-06-17 14:40:52 +0900

    Initial commit

---
+++ Carrier_Simulator/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using log4net;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carrier_Simulator.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        static readonly ILog Logger =
+           LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public MainViewModel()
+        {
+            Logger.Info("Carrier Simulator Start");
+        }
+    }
+}
